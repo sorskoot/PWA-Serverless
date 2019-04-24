af2b856346d488044601fd6cc0f05c82ddbd6327
Timmy Kokke <mental_c@hotmail.com>
2019-04-25 00:41:46 +0200

    added Azure funtion

---
+++ GetScores/index.js
@@ -0,0 +1,23 @@
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: [{
+        date: '04-06-2019 11:45:23',
+        score: 8,
+        name: 'Team Joe'
+      },
+      {
+        date: '04-12-2019 14:15:12',
+        score: 23,
+        name: 'Team Hanna'
+      },
+      {
+        date: '04-12-2019 15:05:45',
+        score: 42,
+        name: 'Team Green'
+      }
+    ]
+  };
+};
--- src/services/score.service.js
+++ src/services/score.service.js
@@ -3,10 +3,14 @@ export const scoreService = {
   };
 
 function getList(){
-    return new Promise((res,rej)=>{
-        res([
-            {date:'04-06-2019 11:45:23',score:8, name:'Team Joe'},
-            {date:'04-12-2019 14:15:12',score:23, name:'Team Hanna'},
-            {date:'04-12-2019 15:05:45',score:42, name:'Team Green'}]);
-    })
+    return fetch('https://pwaserverless.azurewebsites.net/api/GetScores',{
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+      })
+    .then(response=>{
+        return response.json();
+    });
+    
 }
