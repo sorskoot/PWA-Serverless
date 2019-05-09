7d2bad4ca6246b54b6ff475c7298c19a291e788e
Timmy Kokke <mental_c@hotmail.com>
2019-05-09 21:53:12 +0200

    added Counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="counter">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: "counter"
+}
+</script>
+
+<style scoped lang="scss">
+.counter {
+ 
+}
+</style>
--- src/lib/router.js
+++ src/lib/router.js
@@ -1,8 +1,10 @@
 import VueRouter from 'vue-router';
 import Overview from '../components/Overview.vue';
+import Counter from '../components/Counter.vue';
 
 let routes = 
-[{ path: '/', component: Overview }]
+[{ path: '/', component: Overview },
+ { path: '/counter', component: Counter }]
 
 export const router = new VueRouter({
     routes
